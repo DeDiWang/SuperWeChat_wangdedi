6efac9c45cc0fbe646d78ae9cd7df2221fb77d32
王德帝 <1103930991@qq.com>
2016-11-04 11:43:10 +0800

    添加管理Activity的工具类

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/utils/ExitActivityUtils.java
@@ -0,0 +1,33 @@
+package cn.ucai.superwechat.utils;
+
+import android.app.Activity;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by 11039 on 2016/11/4.
+ */
+public class ExitActivityUtils {
+    private List<Activity> activityList = new LinkedList<>();
+
+    private static ExitActivityUtils instance = new ExitActivityUtils();
+
+    private ExitActivityUtils(){}
+
+    private ExitActivityUtils getInstance(){
+        return instance;
+    }
+
+    public void addActivity(Activity activity){
+        activityList.add(activity);
+    }
+    public void delActivity(Activity activity){
+        activityList.remove(activity);
+    }
+    public void exit(){
+        for(Activity activity:activityList){
+            activity.finish();
+        }
+    }
+}
